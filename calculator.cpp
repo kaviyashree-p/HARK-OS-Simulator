e87d8353877e432834bc8e966ecccac220363372
kaviyashree-p <kaviyashree730@gmail.com>
2026-09-08 05:05:16 +0530

    Updated calculator with overloading

--- calculator.cpp
+++ calculator.cpp
@@ -18,6 +18,14 @@ public:
         result = 0;
     }
 
+    // Parameterized Constructor
+    Calculator(double a, double b)
+    {
+        num1 = a;
+        num2 = b;
+        result = 0;
+    }
+
     // Function to get input from user
     void getInput()
     {
@@ -32,7 +40,6 @@ public:
     void add()
     {
         result = num1 + num2;
-
         cout << "Result = " << result << endl;
     }
 
@@ -40,7 +47,6 @@ public:
     void subtract()
     {
         result = num1 - num2;
-
         cout << "Result = " << result << endl;
     }
 
@@ -48,7 +54,6 @@ public:
     void multiply()
     {
         result = num1 * num2;
-
         cout << "Result = " << result << endl;
     }
 
@@ -62,9 +67,40 @@ public:
         else
         {
             result = num1 / num2;
-
             cout << "Result = " << result << endl;
         }
+    }
+
+    // FUNCTION OVERLOADING
+    // calculate() with 2 parameters
+    double calculate(double a, double b)
+    {
+        return a + b;
+    }
+
+    // calculate() with 3 parameters
+    double calculate(double a, double b, double c)
+    {
+        return a + b + c;
+    }
+
+    // OPERATOR OVERLOADING
+    // Overloading + operator
+    Calculator operator+(Calculator obj)
+    {
+        Calculator temp;
+
+        temp.num1 = num1 + obj.num1;
+        temp.num2 = num2 + obj.num2;
+
+        return temp;
+    }
+
+    // Display values
+    void display()
+    {
+        cout << "First number = " << num1 << endl;
+        cout << "Second number = " << num2 << endl;
     }
 
     // Destructor
@@ -82,7 +118,6 @@ int main()
     int choice;
     char again;
 
-
     do
     {
         cout << endl;
@@ -94,7 +129,9 @@ int main()
         cout << "2. Subtraction" << endl;
         cout << "3. Multiplication" << endl;
         cout << "4. Division" << endl;
-        cout << "5. Exit" << endl;
+        cout << "5. Function Overloading" << endl;
+        cout << "6. Operator Overloading" << endl;
+        cout << "7. Exit" << endl;
 
         cout << "Enter your choice: ";
         cin >> choice;
@@ -104,7 +141,6 @@ int main()
         {
             // Get numbers from USER
             calc.getInput();
-
 
             switch(choice)
             {
@@ -126,12 +162,54 @@ int main()
             }
         }
 
+
+        // FUNCTION OVERLOADING
         else if(choice == 5)
         {
-            cout << "Exiting calculator..." << endl;
+            double a, b, c;
 
+            cout << "\nEnter two numbers: ";
+            cin >> a >> b;
+
+            cout << "calculate(a,b) = "
+                 << calc.calculate(a, b) << endl;
+
+            cout << "\nEnter three numbers: ";
+            cin >> a >> b >> c;
+
+            cout << "calculate(a,b,c) = "
+                 << calc.calculate(a, b, c) << endl;
+        }
+
+
+        // OPERATOR OVERLOADING
+        else if(choice == 6)
+        {
+            double a, b, c, d;
+
+            cout << "\nEnter first number pair: ";
+            cin >> a >> b;
+
+            cout << "Enter second number pair: ";
+            cin >> c >> d;
+
+            Calculator calc1(a, b);
+            Calculator calc2(c, d);
+
+            Calculator calc3 = calc1 + calc2;
+
+            cout << "\nAfter Operator Overloading (+):" << endl;
+
+            calc3.display();
+        }
+
+
+        else if(choice == 7)
+        {
+            cout << "Exiting calculator..." << endl;
             break;
         }
+
 
         else
         {
@@ -139,7 +217,7 @@ int main()
         }
 
 
-        if(choice != 5)
+        if(choice != 7)
         {
             cout << "\nDo you want to calculate again? (Y/N): ";
             cin >> again;
@@ -151,5 +229,7 @@ int main()
 
     } while(again == 'Y' || again == 'y');
 
+
     return 0;
 }
+
